29a05252c0c7e3b2309c4ee563a78dddd6ac3e25
presweety <81914816+presweety@users.noreply.github.com>
2021-04-11 22:53:10 +0800

    second
    设置了查询窗口的背景

--- hello'.py
+++ hello'.py
@@ -3,7 +3,7 @@
 # __author__ = 'DELL'
 '''
 import tkinter as tk
-from tkinter import *
+from tkinter import *     #thus we can use its function
 from tkinter import messagebox
 from url import lunar
 
@@ -13,11 +13,11 @@ class visual(tk.Frame):
         super().__init__(window)
         self.master = window
         self.pack()
-        self.date = tk.StringVar()
+        self.date = tk.StringVar(None,'xxxx_xx_xx')
         window.geometry('500x300')
         window.title('中国老黄历查询窗口')
 
-        self.intWind()
+        self.intWindow()
 
     def buttonActive(self):
         p = self.date.get()
@@ -41,16 +41,25 @@ class visual(tk.Frame):
         #       messagebox.showinfo('',data)
         return
 
-    def intWind(self):
+    def intWindow(self):
         frame1 = Frame(self)
-        Label(frame1, text='请输入一个公历日期').grid(row=0, column=0)
-        Entry(frame1, textvariable=self.date).grid(row=0, column=1)
+        Label(frame1, text='请输入一个公历日期',font=('Courier',15)).grid(row=0, column=0)   #add font setting
+        Entry(frame1, textvariable=self.date,font=('Courier',15)).grid(row=1, column=1)
         frame1.grid(pady=10)
-        Button(self, text='确定', width=15, command=self.buttonActive).grid(pady=5)
+        Button(self, text='确定', width=15,font=('Courier',15), command=self.buttonActive).grid(pady=5)
 
 
 if __name__ == '__main__':
     window = tk.Tk()
+
+
+    Canvas = tk.Canvas(window, height=100, width=100)  #add window background
+    Canvas.pack()
+
+    background_image = tk.PhotoImage(file='D:\hhhh.png')
+    background_label = tk.Label(window, image=background_image)
+    background_label.place(relwidth=1, relheight=1)
+
     application = visual(window)
     application.mainloop()
 
